1f1448ea8aa9e6f8f849fce690e75ac337d83f31
Léopold Mittelberger <leopold.mittelberger@gmail.com>
2023-04-12 14:00:53 +0200

    - finished base methods - finished doc for all existing classes - TODO: Dialog, ErrorCheck, JUnit???

--- Übung13/untitled/src/Mitarbeiter.java
+++ Übung13/untitled/src/Mitarbeiter.java
@@ -1,24 +1,57 @@
 public class Mitarbeiter extends Person {
 
+    /**
+     * Attribute für Mitarbeiter entnommen aus UML-Diagramm
+     */
     private String email;
 
+    /**
+     * Kosntruktor für Mitarbeiter
+     * @param vorname vorname des Mitarbeiters
+     * @param nachname Nacghnamen des Mitarbeiters
+     * @param email E-Mail des Mitarbeiters
+     */
     public Mitarbeiter(String vorname, String nachname, String email) {
         super(vorname, nachname);
         this.email = email;
     }
 
+    /**
+     * Methode zum Reservieren eines Raums
+     * @param raum raum der reserviert werden soll
+     * @param begin beginn der reservierung
+     * @param ende ende der reservierung
+     * @param bemerkung bemerkung (wer hat reserviert?)
+     */
+    public void reserviere(Raum raum, Uhrzeit begin, Uhrzeit ende, String bemerkung) {
+        Reservierung reservierung = new Reservierung(begin, ende);
+        reservierung.setMitarbeiter(this);
+        reservierung.setRaum(raum);
+        reservierung.setBemerkung(bemerkung);
+    }
 
-
+    /**
+     * Getter für Email
+     * @return email des Mitarbeiters
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter für Email
+     * @param email welche email soll gesetzt werden
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Methode die den Mitarbeiter als String zurückgibt
+     * @return mitarbeiter als String
+     */
     @Override
     public String toString() {
-        return getVorname() + " " + getNachname() + " (" + email + ")";
+        return super.toString() + " (" + email + ")";
     }
 }
--- Übung13/untitled/src/Uhrzeit.java
+++ Übung13/untitled/src/Uhrzeit.java
@@ -1,34 +1,61 @@
 public class Uhrzeit {
 
+    /**
+     *Attribute für Uhrzeit entnommen aus UML-Diagramm
+     */
     private int stunde;
     private int minute;
 
+    /**
+     * Konstruktor für Uhrzeit
+     * @param stunde welche Stunde
+     * @param minute welche Minute
+     */
     public Uhrzeit(int stunde, int minute) {
         this.stunde = stunde;
         this.minute = minute;
     }
 
+    /**
+     * Getter für Stunde
+     * @return stunde
+     */
     public int getStunde() {
         return stunde;
     }
 
+    /**
+     * setter für Stunde
+     * @param stunde auf welcehe soll gestellt werden
+     */
     public void setStunde(int stunde) {
         this.stunde = stunde;
     }
 
+    /**
+     * Getter für Minute
+     * @return welche Minute
+     */
     public int getMinute() {
         return minute;
     }
 
+    /**
+     * Setter für Minute
+     * @param minute auf welche minute soll gestellt werden
+     */
     public void setMinute(int minute) {
         this.minute = minute;
     }
 
-
-
+    /**
+     * toString Methode gibt Uhrzeit als String zurück
+     * @return Uhrzeit als String
+     */
     @Override
     public String toString() {
-        return stunde + ":" + minute + " Uhr";
+        // wenn minute kleiner als 10 ist, wird eine Null davor gesetzt
+        return stunde + ":" + (minute < 10 ? "0" : "") + minute + " Uhr";
     }
 }
 
