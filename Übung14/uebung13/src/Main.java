e41ca5f6f3602704ea363964759fe032bbb1a153
Eyli333 <elisee.brand@gmail.com>
2023-04-21 10:30:12 +0200

    added nano time and messungen in einer datei

--- Übung14/uebung13/src/Main.java
+++ Übung14/uebung13/src/Main.java
@@ -1,11 +1,31 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.JFrame;
+// import org.jfree.chart.ChartPanel;
+// import org.jfree.chart.ChartFactory;
+// import org.jfree.chart.JFreeChart;
+// import org.jfree.data.category.DefaultCategoryDataset;
 
+
+// import com.panayotis.gnuplot.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        JFrame frame = new JFrame("ma fenetre");
+        
         String wort;
+        // GnuPlotProcess gp = new GnuPlotProcess();
+        // Création d’un fileWriter pour écrire dans un fichier
+        FileWriter fileWriter = new FileWriter("messungen.txt", false);
+
+        // Création d’un bufferedWriter qui utilise le fileWriter
+        BufferedWriter writer = new BufferedWriter (fileWriter);
+
         if (args.length > 0) {
             // Eingabestring als Argument übergeben
             wort = args[0];
@@ -17,44 +37,56 @@ public class Main {
 
                     Palindrom palindromRek = new PalindromRekursiv();
                     Palindrom palindromIte = new PalindromIterativ();
-    
-                    if (palindromRek.istPalindrom(wort) && palindromIte.istPalindrom(wort)) {
+
+                    long nanoTime1Rek = System.nanoTime();
+                    Boolean palindromeRekursive = palindromRek.istPalindrom(wort);
+                    long nanoTime2Rek = System.nanoTime();
+                        
+                    long zeitRekNanoSecond = nanoTime2Rek - nanoTime1Rek;
+                    System.out.println("Zeit brauch um mit den Rekursive funktion zu berechen : " + zeitRekNanoSecond + " nano seconds");
+
+                    long nanoTime1Ite = System.nanoTime();
+                    Boolean palindromeIterative = palindromIte.istPalindrom(wort);
+                    long nanoTime2Ite = System.nanoTime();
+                        
+                    long zeitIteNanoSecond = nanoTime2Ite - nanoTime1Ite;
+                    System.out.println("Zeit brauch um mit den Iterative funktion zu berechen : " + zeitIteNanoSecond + " nano seconds");
+
+                    if (palindromeRekursive && palindromeIterative) {
                         System.out.println("Rekursiv >>> Das Wort \"" + wort + "\" ist ein Palindrom.");
                         System.out.println("Iterativ >>> Das Wort \"" + wort + "\" ist ein Palindrom.\n");
                     } else {
                         System.out.println("Rekursiv >>> Das Wort \"" + wort + "\" ist kein Palindrom.");
                         System.out.println("Iterativ >>> Das Wort \"" + wort + "\" ist kein Palindrom.\n");
                     }
+
+                    // ajout d’un texte à notre fichier
+                    writer.write(wort + " : " + zeitRekNanoSecond + ", " + zeitIteNanoSecond);
+
+                    // Retour à la ligne
+                    writer.newLine();
                 }
             } catch (IOException e) {
                 System.err.println("Fehler beim Lesen der Datei: " + e.getMessage());
-                return;
             }
+
+            writer.close();
+
         }
-
-        
-
-
-
-        // Palindrom-Tests
-        // Palindrom[] algorithmen = {
-        //         new PalindromRekursiv(),
-        //         new PalindromIterativ()
-        // };
-
-        // for (Palindrom p : algorithmen) {
-        //     if (p.istPalindrom(wort)) {
-        //         System.out.println("Rekursiv >>> Das Wort \"" + wort + "\" ist ein Palindrom.");
-        //         System.out.println("Iterativ >>> Das Wort \"" + wort + "\" ist ein Palindrom.");
-        //     } else {
-        //         System.out.println("Rekursiv >>> Das Wort \"" + wort + "\" ist kein Palindrom.");
-        //         System.out.println("Iterativ >>> Das Wort \"" + wort + "\" ist kein Palindrom.");
-        //     }
-        // }
-
-        // for (int i = 0; i < algorithmen.length; i++) {
-        //     System.out.println("Rekursiv >>> Das Wort: " + wort + " ist ein Palindrom.");
-        //     System.out.println("Iterativ >>> Das Wort: " + wort + " ist ein Palindrom.");
-        // }
     }
+    // public void plot2d() {
+    //     JGnuplot jg = new JGnuplot();
+    //     Plot plot = new Plot("") {
+    //         {
+    //             xlabel = "x";
+    //             ylabel = "y";
+    //         }
+    //     };
+    //     double[] x = { 1, 2, 3, 4, 5 }, y1 = { 2, 4, 6, 8, 10 }, y2 = { 3, 6, 9, 12, 15 };
+    //     DataTableSet dts = plot.addNewDataTableSet("2D Plot");
+    //     dts.addNewDataTable("y=2x", x, y1);
+    //     dts.addNewDataTable("y=3x", x, y2);
+    //     jg.execute(plot, jg.plot2d);
+    // }
+    
 }
