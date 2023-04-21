b0f41ddbe9679c3b2985aa5c937f8b90126cd4f7
Léopold Mittelberger <98849025+glebiskava@users.noreply.github.com>
2023-04-21 15:04:41 +0200

    inshallah ca marche

---
+++ Übung14/uebung13/src/Palindrom.java
@@ -0,0 +1,9 @@
+/**
+ * @author Leopold Mittelberger, Elisee Brand
+ * @version 1.0
+ * Dies ist ein einfaches Interface für die Palindrom implementierung
+ */
+public interface Palindrom {
+
+    boolean istPalindrom(String wort);
+}
