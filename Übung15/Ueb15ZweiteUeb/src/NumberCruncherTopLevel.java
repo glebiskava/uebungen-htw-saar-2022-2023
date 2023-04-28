9d578fefd8be9ad7a553c00be2cc9acb88813439
Eyli333 <elisee.brand@gmail.com>
2023-04-28 14:58:30 +0200

    begined zweite teil des uebung, and put it in an other directory

---
+++ Übung15/Ueb15ZweiteUeb/src/NumberCruncherTopLevel.java
@@ -0,0 +1,21 @@
+public class NumberCruncherTopLevel {
+    /**
+     * erstellen des Arrays
+     */
+    private float[] zahlen;
+    private int langeArray;
+
+    NumberCruncherTopLevel(float[] zahlen) {
+        this.zahlen = zahlen;
+        // this.langeArray = langue
+        
+    }
+
+    public void crunch(String[] operations){
+
+    }
+
+    public float[] getNumbers(){
+        return zahlen;
+    }
+}
