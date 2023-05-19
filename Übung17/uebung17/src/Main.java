e7d7639140c5cc1aa4f041a9929c8003411d936a
Eyli333 <elisee.brand@gmail.com>
2023-05-20 00:08:42 +0200

    added aufgabe a, b , c

--- Übung17/uebung17/src/Main.java
+++ Übung17/uebung17/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lambda lambda = new Lambda();
+        lambda.applyAndPrint(1, x -> x + x);
     }
 }
