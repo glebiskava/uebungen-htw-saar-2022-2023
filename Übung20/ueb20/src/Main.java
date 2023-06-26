7e2374c1e0eafd6fbbf21afd8acd4dc8d9122bca
Léopold Mittelberger <leopold.mittelberger@gmail.com>
2023-06-26 20:19:56 +0200

    - aufgabe 1 done - problem solved - tests done for aufgabe 1

--- Übung20/ueb20/src/Main.java
+++ Übung20/ueb20/src/Main.java
@@ -26,39 +26,57 @@ public class Main {
         Random ran = new Random();
 
         // wenn ran.nextInt() größer null also 1, dann produce wenn nicht dann consume
-        for (int i = 0; i < 10; i++) {
-            int randomInt = ran.nextInt(2);
+        for (int i = 0; i < 50; i++) {
+            int randomInt = ran.nextInt(2); //zufaelliger Int bis 2 also 0 oder 1
 
-            if (randomInt > 0) { //zufaelliger Int bis 2 also 0 oder 1
+            if (randomInt > 0) {
                 System.out.println("Random Integer : " + randomInt + " --> Produce");
                 int number = producer.produce();
-                // Speichern des erzeugten Integers in der Collection
                 arrayList.add(number);
             } else {
                 System.out.println("Random Integer : " + randomInt + " --> Consume");
-                // Entnehmen eines Integers aus der Collection und Berechnung der Quersumme
                 if (!arrayList.isEmpty()) {
                     int number;
                     if (isFIFO) {
-                        number = arrayList.get(0);
-                        consumer.consume(number);
+                        number = arrayList.remove(0);
                     } else {
                         number = arrayList.get(arrayList.size() - 1);
-                        consumer.consume(number);
+                        arrayList.remove(arrayList.size() - 1);
                     }
-                    int sum = consumer.calculateDigitSum(number);
-                    System.out.println("Quersumme: " + sum);
+
+                    consumer.consume(number);
+
+                } else {
+                    System.out.println("ArrayList is empty. Cannot consume.");
                 }
             }
         }
 
         System.out.println("Ende der Schleife");
 
-        System.out.println("\n\n\nNumber of different results: " + consumer.numberOfDifferentResults());
-        System.out.println("Number of occurrences for number 8: " + consumer.numberOfOccurrences(8));
-        System.out.println("Cross total ascending: " + consumer.getCrossTotalsAscending());
-        System.out.println("Cross total descending: " + consumer.getCrossTotalsDescending());
-        System.out.println("Timestamps for result 14: " + consumer.getTimestampsForResult(14));
+        System.out.println("\n\n\nNumber of different results: "  + consumer.numberOfDifferentResults());
+
+        System.out.println("Cross total ascending: "    + consumer.getCrossTotalsAscending());
+
+        System.out.println("Cross total descending: "   + consumer.getCrossTotalsDescending());
+
+        System.out.println("Number of occurrences for number 8: "       + consumer.numberOfOccurrences(8));
+        System.out.println("Number of occurrences for number 13: "      + consumer.numberOfOccurrences(13));
+        System.out.println("Number of occurrences for number -12: "     + consumer.numberOfOccurrences(-12));
+        System.out.println("Number of occurrences for number 14: "      + consumer.numberOfOccurrences(14));
+        System.out.println("Number of occurrences for number 16: "      + consumer.numberOfOccurrences(16));
+        System.out.println("Number of occurrences for number 4: "       + consumer.numberOfOccurrences(4));
+        System.out.println("Number of occurrences for number 11: "      + consumer.numberOfOccurrences(11));
+        System.out.println("Number of occurrences for number 10: "      + consumer.numberOfOccurrences(10));
+
+        System.out.println("Timestamps for result 1: "                  + consumer.getTimestampsForResult(1));
+        System.out.println("Timestamps for result 12: "                 + consumer.getTimestampsForResult(12));
+        System.out.println("Timestamps for result 10: "                 + consumer.getTimestampsForResult(10));
+        System.out.println("Timestamps for result 7: "                  + consumer.getTimestampsForResult(7));
+        System.out.println("Timestamps for result -5: "                 + consumer.getTimestampsForResult(-5));
+        System.out.println("Timestamps for result 3: "                  + consumer.getTimestampsForResult(3));
+        System.out.println("Timestamps for result 13: "                 + consumer.getTimestampsForResult(13));
+        System.out.println("Timestamps for result 11: "                 + consumer.getTimestampsForResult(11));
 
     }
 }
