a98259b44e6627287ece5efc569835c9657e02ff
Léopold Mittelberger <98849025+glebiskava@users.noreply.github.com>
2023-06-30 08:32:58 +0200

    - aufgabe 2 und 3 done

---
+++ Übung21/fib.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+	
+    int n;
+
+    printf("Geben Sie eine positive Zahl ein : ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Geben Sie eine Zahl größer als 0 ein!");
+    } else {
+        int result = fibonacci(n);
+        printf("Fibonacci fuer %d ==> %d\n", n, result);
+    }
+
+    return 0;
+    
+}
+
+
